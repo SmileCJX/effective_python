8e08c40429df84ad3e295471e961401aa63db964
SmileCJX <1092925421@qq.com>
2018-07-22 23:48:38 +0800

    range example

---
+++ rangeTest.py
@@ -0,0 +1,9 @@
+# 第一个参数是for循环变量开始的值，第二个参数是上限
+for i in range(12,16):
+    print(i)
+# 前两个参数分别是起始值和终止值，第三个参数是步长
+for i in range(0,10,2):
+    print(i)
+# 可以用负数作为步数参数
+for i in range(5,-1,-1):
+    print(i)
