ab14fbaa4b868c4fc0bce46dc0870e39845e59b2
SmileCJX <1092925421@qq.com>
2018-07-23 21:41:50 +0800

    名称相同的局部变量和全局变量

---
+++ pers.caijx.tools/func/sameName.py
@@ -0,0 +1,13 @@
+def spam():
+    eggs = 'Spam local'
+    print(eggs) # prints 'Spam local'
+
+def bacon():
+    eggs = 'bacon local'
+    print(eggs) # prints 'bacon local'
+    spam()
+    print(eggs) # prints 'bacon local'
+
+eggs = 'global'
+bacon()
+print(eggs) # prints 'global'
