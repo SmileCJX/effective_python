165757d8160551d378ea0c290a811ca5601fc6e7
SmileCJX <1092925421@qq.com>
2018-07-26 19:51:30 +0800

    多行注释

---
+++ pers.caijx.tools/string/multiAnnotation.py
@@ -0,0 +1,13 @@
+"""This is a test python program.
+Written by caijx
+
+This program was designed for Python 3, not Python 2.
+"""
+
+def spam():
+    """This is a multiline comment to help
+    explain what the spam() function does.
+    """
+    print('Hello!')
+
+spam()
