acee561cf75097ec9bb43aca9f38e5a578fdd8c2
SmileCJX <1092925421@qq.com>
2018-07-29 23:06:08 +0800

    当前工作目录，处理绝对路径和相对路径

--- pers.caijx.tools/file/currWorkspace.py
+++ pers.caijx.tools/file/currWorkspace.py
@@ -4,3 +4,10 @@ import os
 print(os.getcwd())
 os.chdir('C:\\dev\\IdeaProjects\\effective_python')
 print(os.getcwd())
+
+# 处理绝对路径和相对路径
+print('------------------------')
+print(os.path.abspath('.'))
+print(os.path.abspath('.\\scripts'))
+print(os.path.isabs('.'))
+print(os.path.isabs(os.path.abspath('.')))
