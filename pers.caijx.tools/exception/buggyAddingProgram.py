cb584af8d58d74a6165ee60a5a209e3f1e593f58
SmileCJX <1092925421@qq.com>
2018-08-01 22:07:41 +0800

    调试一个数字相加的程序

---
+++ pers.caijx.tools/exception/buggyAddingProgram.py
@@ -0,0 +1,9 @@
+#! python3
+# buggyAddingProgram.py - 调试一个数字相加的程序
+print('Enter the first number to add:')
+first = input()
+print('Enter the second number to add:')
+second = input()
+print('Enter the third number to add:')
+third = input()
+print('The sum is ' + first + second + third)
