47b3047b51c7b5d0cb7820eb7c9b9ccca99364cf
SmileCJX <1092925421@qq.com>
2018-08-04 01:17:19 +0800

    用select()方法寻找元素

---
+++ pers.caijx.tools/web/selectElement.py
@@ -0,0 +1,21 @@
+#! python3
+# selectElement.py - 用select()方法寻找元素
+import bs4
+exampleFile = open('.\\example.html')
+exampleSoup = bs4.BeautifulSoup(exampleFile.read(),features='html.parser')
+elems = exampleSoup.select('#author')
+print('类型：' + str(type(elems)))
+print('长度：' + str(len(elems)))
+print('类型：' + str(type(elems[0])))
+print(elems[0].getText())
+print(str(elems[0]))
+print(elems[0].attrs)
+
+pElems = exampleSoup.select('p')
+print('长度：' + str(len(pElems)))
+print(str(pElems[0]))
+print(pElems[0].getText())
+print(str(pElems[1]))
+print(pElems[0].getText())
+print(str(pElems[2]))
+print(pElems[2].getText())
