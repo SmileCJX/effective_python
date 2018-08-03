e46c518531b3d4000c83811bbb8942182af9d8c7
SmileCJX <1092925421@qq.com>
2018-08-04 01:52:25 +0800

    打开百度搜索的前5条

--- pers.caijx.tools/web/lucky.py
+++ pers.caijx.tools/web/lucky.py
@@ -1,12 +1,16 @@
 #! python3
-# lucky.py - 打开百度搜索结果
+# lucky.py - 打开百度搜索结果的前5条
 import requests,sys,webbrowser,bs4
 print('Baidu ing ...') # display text while downloading the baidu page
 res = requests.get('http://www.baidu.com/s?wd=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 # Retrive top search result links.
-soup = bs4.BeautifulSoup(res.text)
+soup = bs4.BeautifulSoup(res.text,features='html.parser')
 
-# Open a browser tab for each result.
-linkElems = soup.select('.result.c-container>.t>a')
+# Open a browser tab for each result.r
+linkElems = soup.select('.result.c-container > h3 > a')
+print(str(len(linkElems)))
+numOpen = min(5,len(linkElems))
+for i in range(numOpen):
+    webbrowser.open(linkElems[i].get('href'))
