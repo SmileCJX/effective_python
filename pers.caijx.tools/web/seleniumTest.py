c3e26943f99e55bb597a5c96a27a313faddf6056
SmileCJX <1092925421@qq.com>
2018-08-04 15:06:16 +0800

    找到带有container类名的标签名

--- pers.caijx.tools/web/seleniumTest.py
+++ pers.caijx.tools/web/seleniumTest.py
@@ -4,4 +4,11 @@
 from selenium import webdriver
 browser = webdriver.Chrome()
 print(str(type(browser)))
-browser.get('http://www.baidu.com')
+browser.get('http://www.zhibo8.com')
+
+# 找到带有container类名的标签名
+try:
+    elem = browser.find_element_by_class_name('container')
+    print('Found <%s> element with that class name!' % (elem.tag_name))
+except:
+    print('Was not able to find an element with that name.')
