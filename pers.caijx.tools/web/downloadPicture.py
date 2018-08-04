3fadbc1ebbba54e67ec40d3d21aa86519e58767b
SmileCJX <1092925421@qq.com>
2018-08-04 14:00:45 +0800

    下载网页

--- pers.caijx.tools/web/downloadPicture.py
+++ pers.caijx.tools/web/downloadPicture.py
@@ -6,7 +6,11 @@ url = 'http://xkcd.com'  # starting url
 os.makedirs('./xkcd',exist_ok=True) # store comics in ./xkcd
 while not url.endswith('#'):
     # Download the page.
+    print('Downloading page %s...' % url)
+    res = requests.get(url)
+    res.raise_for_status()
 
+    soup = bs4.BeautifulSoup(res.text)
     # Find the URL of the comic image.
 
     # Download the image.
