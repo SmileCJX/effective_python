e875c5cd9172848f8fc7afaf2ff0bdb1a54c6f8f
SmileCJX <1092925421@qq.com>
2018-08-05 22:52:22 +0800

    从表中取得单元格

---
+++ pers.caijx.tools/excel/cellExample.py
@@ -0,0 +1,12 @@
+#! python3
+# cellExample.py - 从表中取得单元格
+import openpyxl
+wb = openpyxl.load_workbook('.\\example.xlsx')
+sheet = wb['Sheet1']
+print(sheet['A1'])
+print(sheet['A1'].value)
+c = sheet['B1']
+print(c.value)
+print('Row ' + str(c.row) + ',Column ' + c.column + ' is ' + c.value)
+print('Cell ' + c.coordinate + ' is ' + c.value)
+print(sheet['C1'].value)
