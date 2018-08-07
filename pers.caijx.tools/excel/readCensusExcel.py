749fbf8441b92f7a1c0a0f61e4ee0fc83e4e6f92
SmileCJX <1092925421@qq.com>
2018-08-07 19:03:54 +0800

    填充数据结构

--- pers.caijx.tools/excel/readCensusExcel.py
+++ pers.caijx.tools/excel/readCensusExcel.py
@@ -13,3 +13,14 @@ for row in range(2,sheet.max_row + 1):
     state = sheet['B' + str(row)].value
     country = sheet['C' + str(row)].value
     pop = sheet['D' + str(row)].value
+    # make sure the key for this state exists.
+    countryData.setdefault(state,{})
+    # make sure the key for this country in this state exists.
+    countryData[state].setdefault(country,{'tracts':0,'pop':0})
+    # Each row represents one census tract,so increment by one.
+    countryData[state][country]['tracts'] += 1
+    # increase the country pop by the pop in this census tract.
+    countryData[state][country]['pop'] += int(pop)
+
+# Open a new text file and write the contents of countryData to it.
+
