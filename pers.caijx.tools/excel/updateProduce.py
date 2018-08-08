7f37fcda67673904a7de01da7c727b632995a115
SmileCJX <1092925421@qq.com>
2018-08-09 00:30:29 +0800

    检查所有行，更新不正确的价格

--- pers.caijx.tools/excel/updateProduce.py
+++ pers.caijx.tools/excel/updateProduce.py
@@ -9,3 +9,9 @@ PRICE_UPDATES = {
     'Celery':1.19,
     'Lemon':1.27
 }
+# Loop through the rows and update the prices.
+for rowNum in range(2,sheet.max_row): # skip the first row
+    produceName = sheet.cell(row=rowNum,column=1).value
+    if produceName in PRICE_UPDATES:
+        sheet.cell(row=rowNum,column=2).value = PRICE_UPDATES[produceName]
+wb.save('.\\updatedProdeceSales.xlsx')
