64dc797a69290c361f0f2e439ed42e3997137757
SmileCJX <1092925421@qq.com>
2018-08-09 22:11:45 +0800

    图表

---
+++ pers.caijx.tools/excel/chartExample.py
@@ -0,0 +1,15 @@
+#! python3
+# chartExample.py - 图表
+import openpyxl
+wb = openpyxl.Workbook()
+sheet = wb.active
+for i in range(1,11): # create some data in column A
+    sheet['A' + str(i)] = i
+refObj = openpyxl.chart.Reference(sheet,min_row = 1,min_col = 1,max_row = 10,max_col = 1)
+seriesObj = openpyxl.chart.Series(refObj,title='First series')
+chartObj = openpyxl.chart.BarChart()
+
+chartObj.title = 'My Chart'
+chartObj.append(seriesObj)
+sheet.add_chart(chartObj,'C5')
+wb.save('.\\sampleChart.xlsx')
