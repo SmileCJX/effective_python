5b9890238d46b1d697e2191685b994c6847567c4
SmileCJX <1092925421@qq.com>
2018-08-12 22:55:54 +0800

    添加换行符和换页符

---
+++ pers.caijx.tools/word/docxBreak.py
@@ -0,0 +1,8 @@
+#! python3
+# docxBreak.py - 添加换行符和换页符
+import docx
+doc = docx.Document()
+doc.add_paragraph('This is on the first page!')
+doc.paragraphs[0].runs[0].add_break(docx.enum.text.WD_BREAK.PAGE)
+doc.add_paragraph('This is on the second page!')
+doc.save('.\\twoPage.docx')
