f1950cca6d9514f5fe43d20709a5a692360cc707
SmileCJX <1092925421@qq.com>
2018-08-12 21:06:53 +0800

    找到所有pdf文件夹

---
+++ pers.caijx.tools/pdf/combinePdfs.py
@@ -0,0 +1,16 @@
+#! python3
+# combinePdfs.py - 从多个pdf中合并选择的页面
+import PyPDF2,os
+# Get all the PDF filenames.
+pdfFiles = []
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+pdfFiles.sort(key=str.lower)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+# Loop through all the pdf files.
+
+# Loop through all the pages (except the first) and add them.
+
+# Save the resulting PDF to a file
