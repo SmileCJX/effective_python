90f424a25a26ee5fc3622f3c2092be7adef5e107
SmileCJX <1092925421@qq.com>
2018-08-12 22:50:05 +0800

    添加标题

---
+++ pers.caijx.tools/word/docxHeader.py
@@ -0,0 +1,10 @@
+#! python3
+# docxHeader.py - 添加标题
+import docx
+doc = docx.Document()
+doc.add_heading('Header 0',0)
+doc.add_heading('Header 1',1)
+doc.add_heading('Header 2',2)
+doc.add_heading('Header 3',3)
+doc.add_heading('Header 4',4)
+doc.save('.\\heading.docx')
