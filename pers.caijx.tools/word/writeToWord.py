ce2ba2dd845ee09259df1b18413d8240498d41f8
SmileCJX <1092925421@qq.com>
2018-08-12 22:47:13 +0800

    写入word文档

--- pers.caijx.tools/word/writeToWord.py
+++ pers.caijx.tools/word/writeToWord.py
@@ -4,3 +4,7 @@ import docx
 doc = docx.Document()
 doc.add_paragraph('hello_world!')
 doc.save('.\\helloworld.docx')
+paraObj1 = doc.add_paragraph('This is a second paragraph.')
+paraObj2 = doc.add_paragraph('This is a yet another paragraph.')
+paraObj1.add_run('This text is being added to the second paragraph.')
+doc.save('.\\multiParagraphs.docx')
