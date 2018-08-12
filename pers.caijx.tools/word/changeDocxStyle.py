9d5c98164b91fc8c6ebed8127ca028c17bba8303
SmileCJX <1092925421@qq.com>
2018-08-12 22:37:39 +0800

    run属性

---
+++ pers.caijx.tools/word/changeDocxStyle.py
@@ -0,0 +1,13 @@
+#! python3
+# changeDocxStyle.py - Run属性
+import docx
+doc = docx.Document('.\\demo.docx')
+print(doc.paragraphs[0].text)
+print(doc.paragraphs[0].style)
+doc.paragraphs[0].style = 'Normal'
+print(doc.paragraphs[1].text)
+(doc.paragraphs[1].runs[0].text, doc.paragraphs[1].runs[1].text, doc.paragraphs[1].runs[2].text, doc.paragraphs[1].runs[3].text)
+doc.paragraphs[1].runs[0].style = 'Quotechar'
+doc.paragraphs[1].runs[1].underline = 'True'
+doc.paragraphs[1].runs[3].underline = 'True'
+doc.save('.\\restyled.docx')
