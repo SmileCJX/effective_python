0d12a6d7740d45eadfdcbb837e2cc4f943f21f0d
SmileCJX <1092925421@qq.com>
2018-08-13 21:42:01 +0800

    读入CSV文件

--- pers.caijx.tools/csvAndJson/removeCsvHeader.py
+++ pers.caijx.tools/csvAndJson/removeCsvHeader.py
@@ -7,9 +7,14 @@ os.makedirs('headerRemoved',exist_ok=True)
 for csvFilename in os.listdir('.'):
     if not csvFilename.endswith('.csv'):
         continue # skip non-csv files
-
+        csvRows = []
+        csvFileObj = open(csvFilename)
+        readerObj = csv.reader(csvFileObj)
+        for row in readerObj:
+            if readerObj.line_num == 1:
+                continue # skip first row
+            csvRows.append(row)
+        csvFileObj.close()
     print('Removing header from ' + csvFilename + '...')
-
-    # Read the CSV file in (skiping first row).
 
     # write out the CSV file.
