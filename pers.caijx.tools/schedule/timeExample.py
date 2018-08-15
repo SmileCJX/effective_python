2b35093407d8d092218c5ad296b6fe7b7dabf0ca
SmileCJX <1092925421@qq.com>
2018-08-16 00:00:30 +0800

    计算程序用时

--- pers.caijx.tools/schedule/timeExample.py
+++ pers.caijx.tools/schedule/timeExample.py
@@ -1,4 +1,16 @@
 #! python3
 # timeExample.py - time模块
 import time
-print(time.time())
+def calcProd():
+    # Calculate the product of the first 100000 numbers.
+    product = 1
+    for i in range(1,10000):
+        product = product * i;
+    return product
+
+startTime = time.time()
+prod = calcProd()
+endTime = time.time()
+print('The result is %s digits long.' % (len(str(prod))))
+print('Took %s seconds to calculate.' % (endTime - startTime))
+# print(time.time())
