52d1599fd10e6dda59fa5086840f069de2514566
SmileCJX <1092925421@qq.com>
2018-08-17 13:34:02 +0800

    计算当前日期后的1000天

--- pers.caijx.tools/schedule/timedeltaExample.py
+++ pers.caijx.tools/schedule/timedeltaExample.py
@@ -5,3 +5,9 @@ delta = datetime.timedelta(days=11,hours=10,minutes=9,seconds=8)
 print(str(delta.days) + '天' + str(delta.seconds) + '秒' + str(delta.microseconds) + '微秒')
 print(str(delta.total_seconds()) + '秒')
 print(str(delta))
+
+# 计算当前时间后的1000天的日期
+dt = datetime.datetime.now()
+print(dt)
+thousandDays = datetime.timedelta(days=1000)
+print(dt + thousandDays)
