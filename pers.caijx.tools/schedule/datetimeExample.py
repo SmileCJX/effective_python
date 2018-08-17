9ab1cc2d523abc7c6989d90da9e6cdbe013707b8
SmileCJX <1092925421@qq.com>
2018-08-17 13:20:08 +0800

    比较时间

--- pers.caijx.tools/schedule/datetimeExample.py
+++ pers.caijx.tools/schedule/datetimeExample.py
@@ -8,3 +8,12 @@ print(str(dt.hour) + '时' + str(dt.minute) + '分' + str(dt.second) + '秒')
 
 print(datetime.datetime.fromtimestamp(1000000))
 print(datetime.datetime.fromtimestamp(time.time()))
+
+# 比较时间
+halloween2015 = datetime.datetime(2015,10,31,0,0,0)
+newYear2018 = datetime.datetime(2018,8,17,13,18,0)
+oct31_2015 = datetime.datetime(2015,10,31,0,0,0)
+print(halloween2015 == oct31_2015)
+print(halloween2015 > newYear2018)
+print(newYear2018 > halloween2015)
+print(newYear2018 != oct31_2015)
