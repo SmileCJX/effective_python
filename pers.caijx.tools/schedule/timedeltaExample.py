3ee8ad44294e686fa7b82795d2bef52567d50aa4
SmileCJX <1092925421@qq.com>
2018-08-17 13:48:11 +0800

    暂停直至特定日期

--- pers.caijx.tools/schedule/timedeltaExample.py
+++ pers.caijx.tools/schedule/timedeltaExample.py
@@ -1,6 +1,6 @@
 #! python3
 # timedeltaExample.py - timedelta数据类型
-import datetime
+import datetime,time
 delta = datetime.timedelta(days=11,hours=10,minutes=9,seconds=8)
 print(str(delta.days) + '天' + str(delta.seconds) + '秒' + str(delta.microseconds) + '微秒')
 print(str(delta.total_seconds()) + '秒')
@@ -18,3 +18,8 @@ aboutThirtyYears = datetime.timedelta(days=365 * 30)
 print(aboutThirtyYears)
 print(oct21st - aboutThirtyYears)
 print(oct21st - (2 * aboutThirtyYears))
+
+# 暂停直至特定日期
+aug2018 = datetime.datetime(2018,8,17,13,50,0)
+while datetime.datetime.now() < aug2018:
+    time.sleep(1)
