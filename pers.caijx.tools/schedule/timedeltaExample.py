49bad69f1fd207f81055acfd084319e857a66a03
SmileCJX <1092925421@qq.com>
2018-08-17 13:39:28 +0800

    计算30年前的某一天以及60年前的某一天

--- pers.caijx.tools/schedule/timedeltaExample.py
+++ pers.caijx.tools/schedule/timedeltaExample.py
@@ -11,3 +11,10 @@ dt = datetime.datetime.now()
 print(dt)
 thousandDays = datetime.timedelta(days=1000)
 print(dt + thousandDays)
+
+# 计算30年前的某一天以及60年前的某一天
+oct21st = datetime.datetime(2015,10,21,16,29,0)
+aboutThirtyYears = datetime.timedelta(days=365 * 30)
+print(aboutThirtyYears)
+print(oct21st - aboutThirtyYears)
+print(oct21st - (2 * aboutThirtyYears))
