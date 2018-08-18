efc20c1803cae60562d5b51fd6bf3591a5c2920c
SmileCJX <1092925421@qq.com>
2018-08-18 17:50:55 +0800

    将字符串对象转换成datetime对象

---
+++ pers.caijx.tools/schedule/str2datetime.py
@@ -0,0 +1,7 @@
+#! python3
+# str2datetime.py - 将字符串转换成datetime对象
+import datetime
+print(datetime.datetime.strptime('October 21, 2015','%B %d, %Y'))
+print(datetime.datetime.strptime('2015/10/21 16:29:00', '%Y/%m/%d %H:%M:%S'))
+print(datetime.datetime.strptime('October of \'15\'','%B of \'%y\''))
+print(datetime.datetime.strptime("November of '63", "%B of '%y"))
