69a30bf6b47deead87f801ccc1f815d843293e66
SmileCJX <1092925421@qq.com>
2018-08-18 22:33:11 +0800

    播放音乐文件

--- pers.caijx.tools/thread/countdown.py
+++ pers.caijx.tools/thread/countdown.py
@@ -1,10 +1,11 @@
 #! python3
 # countdown.py - 倒计时
 import time,subprocess
-timeLeft = 60
+timeLeft = 20
 while timeLeft > 0:
     print(timeLeft,end='')
     time.sleep(1)
     timeLeft = timeLeft - 1
 
 # At the end of countdown,play a sound file.
+subprocess.Popen(['start','末日不孤单.mp3'],shell=True)
