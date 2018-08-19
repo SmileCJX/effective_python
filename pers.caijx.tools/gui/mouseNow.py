51e879baf1eb70ecc9b774570bb6e9b4ec75c685
SmileCJX <1092925421@qq.com>
2018-08-19 11:23:38 +0800

    实时得到鼠标的位置

--- pers.caijx.tools/gui/mouseNow.py
+++ pers.caijx.tools/gui/mouseNow.py
@@ -1,6 +1,14 @@
 #! python3
 # mouseMouse.py - 实时得到鼠标的位置
+# todo 未能清除原先的坐标
 import pyautogui
 print('Press CTRL-C to quit.')
-
-# Get and print the mouse coordinates.
+try:
+    while True:
+        # Get and print the mouse coordinates.
+        x,y = pyautogui.position()
+        positionStr = 'X:' + str(x).rjust(4) + ' Y:' + str(y).rjust(4)
+        print(positionStr,end='')
+        print('\b' * len(positionStr),end='',flush=True)
+except KeyboardInterrupt:
+    print('\nDone.')
