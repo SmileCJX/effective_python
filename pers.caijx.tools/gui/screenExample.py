46d8a326d119929359fe1bfe60d1cf14c241a151
SmileCJX <1092925421@qq.com>
2018-08-20 18:15:06 +0800

    分析屏幕快照

--- pers.caijx.tools/gui/screenExample.py
+++ pers.caijx.tools/gui/screenExample.py
@@ -4,3 +4,6 @@ import pyautogui
 im = pyautogui.screenshot()
 print(im.getpixel((0,0)))
 print(im.getpixel((50,200)))
+# 分析屏幕快照
+print(pyautogui.pixelMatchesColor(50, 200, (130, 135, 144)))
+print(pyautogui.pixelMatchesColor(50, 200, (60, 63, 65)))
