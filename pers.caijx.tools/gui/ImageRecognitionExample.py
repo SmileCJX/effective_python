3b48afc1979ef1e80eb31380d6bb1815dee8be0f
SmileCJX <1092925421@qq.com>
2018-08-20 18:35:37 +0800

    图像识别以及中心点

--- pers.caijx.tools/gui/ImageRecognitionExample.py
+++ pers.caijx.tools/gui/ImageRecognitionExample.py
@@ -2,4 +2,6 @@
 # ImageRecognitionExample.py - 图像识别
 import pyautogui
 print(pyautogui.locateOnScreen('.\\test.png'))
-print(list(pyautogui.locateOnScreen('.\\multiTest.png')))  # 如果该图像可以在屏幕中找到多处，则可以得到一个列表
+# print(list(pyautogui.locateOnScreen('.\\multiTest.png')))  # 如果该图像可以在屏幕中找到多处，则可以得到一个列表
+print('中心点：' + str(pyautogui.center((265, 375, 166, 82))))
+pyautogui.click((348, 416))
