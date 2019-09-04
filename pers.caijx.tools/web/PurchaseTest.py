bc61ae00d0db581b22cb4d2cee68a03499cc4a84
caijx <1092925421@qq.com>
2019-09-04 23:15:13 +0800

    将招标结果采集到Excel中

--- pers.caijx.tools/web/PurchaseTest.py
+++ pers.caijx.tools/web/PurchaseTest.py
@@ -21,27 +21,29 @@ wbk = xlwt.Workbook(encoding='utf-8', style_compression=0)
 sheet = wbk.add_sheet('采购信息', cell_overwrite_ok=True)
 
 try:
+    browser.get('http://zfcg.fuzhou.gov.cn/350100/noticelist/e8d2cd51915e4c338dc1c6ee2f02b127/?page=1&notice_type=b716da75fe8d4e4387f5a8c72ac2a937&croporgan_name=%E5%8C%BB%E9%99%A2')
+    # 表头（table_top_list包含表头每一列的值）
+    table_top_list = browser.find_element_by_xpath("//table/thead/tr").find_elements_by_tag_name('td')
+    for c, top in enumerate(table_top_list):
+        sheet.write(0, c, top.text)
+        # print(top.text)
+
     # 循环执行 进行分页的请求
     for i in range(10):
-        browser.get('http://zfcg.fuzhou.gov.cn/350100/noticelist/e8d2cd51915e4c338dc1c6ee2f02b127/?page=' + str(i) +'&notice_type=b716da75fe8d4e4387f5a8c72ac2a937&croporgan_name=%E5%8C%BB%E9%99%A2')
-        # 表头（table_top_list包含表头每一列的值）
-        table_top_list = browser.find_element_by_xpath("//table/thead/tr").find_elements_by_tag_name('td')
-        for c, top in enumerate(table_top_list):
-            sheet.write(0, c, top.text)
-            # print(top.text)
+        browser.get('http://zfcg.fuzhou.gov.cn/350100/noticelist/e8d2cd51915e4c338dc1c6ee2f02b127/?page=' + str(i + 1) +'&notice_type=b716da75fe8d4e4387f5a8c72ac2a937&croporgan_name=%E5%8C%BB%E9%99%A2')
         # 表的内容
         # 将表的每一行存在table_tr_list中
         table_tr_list = browser.find_element_by_xpath("//table/tbody").find_elements_by_tag_name('tr')
-        for r, tr in enumerate(table_tr_list, 1):
+        for r, tr in enumerate(table_tr_list, 0):
             # 将表的每一行的每一列内容存在table_td_list中
             table_td_list = tr.find_elements_by_tag_name('td')
             # 写入表的内容到sheet 1中，第r行第c列
             for c, td in enumerate(table_td_list):
-                sheet.write(r, c, td.text)
-                if 3 == c:
+                sheet.write(r + i * 10, c, td.text)
+                # if 3 == c:
                     # 页面跳转
-                    linkElem = browser.find_element_by_link_text(td.text)
-                    linkElem.click()
+                    # linkElem = browser.find_element_by_link_text(td.text)
+                    # linkElem.click()
                 # print(td.text)
         # elem = browser.find_element_by_class_name('wrapTable')
         # print('Found <%s> element with that class name!' % (elem.tag_name))
