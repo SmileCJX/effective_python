ebb86923ac5f082c4acd67148e4939dcb6602c5a
caijx <1092925421@qq.com>
2019-09-13 22:52:11 +0800

    三明市采购信息

--- pers.caijx.tools/web/PurchaseTest.py
+++ pers.caijx.tools/web/PurchaseTest.py
@@ -27,7 +27,9 @@ try:
     # 厦门市采购信息
     # browser.get('http://202.109.244.105/350200/noticelist/e8d2cd51915e4c338dc1c6ee2f02b127/?page=1&notice_type=b716da75fe8d4e4387f5a8c72ac2a937&croporgan_name=%E5%8C%BB%E9%99%A2')
     # 莆田市采购网
-    browser.get('http://www.ptzfcg.gov.cn/350300/noticelist/e8d2cd51915e4c338dc1c6ee2f02b127/?page=1&notice_type=b716da75fe8d4e4387f5a8c72ac2a937&croporgan_name=%E5%8C%BB%E9%99%A2')
+    # browser.get('http://www.ptzfcg.gov.cn/350300/noticelist/e8d2cd51915e4c338dc1c6ee2f02b127/?page=1&notice_type=b716da75fe8d4e4387f5a8c72ac2a937&croporgan_name=%E5%8C%BB%E9%99%A2')
+    # 三明市采购信息
+    browser.get('http://zfcg.cz.sm.gov.cn/350400/noticelist/e8d2cd51915e4c338dc1c6ee2f02b127/?page=1&croporgan_name=%E5%8C%BB%E9%99%A2&notice_type=b716da75fe8d4e4387f5a8c72ac2a937')
     # 表头（table_top_list包含表头每一列的值）
     table_top_list = browser.find_element_by_xpath("//table/thead/tr").find_elements_by_tag_name('td')
     for c, top in enumerate(table_top_list):
@@ -62,7 +64,9 @@ try:
         # 厦门市采购信息
         # browser.get('http://202.109.244.105/350200/noticelist/e8d2cd51915e4c338dc1c6ee2f02b127/?page=' + str(i + 1) +'&notice_type=b716da75fe8d4e4387f5a8c72ac2a937&croporgan_name=医院')
         # 莆田市采购信息
-        browser.get('http://www.ptzfcg.gov.cn/350300/noticelist/e8d2cd51915e4c338dc1c6ee2f02b127/?page=' + str(i + 1) + '&notice_type=b716da75fe8d4e4387f5a8c72ac2a937&croporgan_name=%E5%8C%BB%E9%99%A2')
+        # browser.get('http://www.ptzfcg.gov.cn/350300/noticelist/e8d2cd51915e4c338dc1c6ee2f02b127/?page=' + str(i + 1) + '&notice_type=b716da75fe8d4e4387f5a8c72ac2a937&croporgan_name=%E5%8C%BB%E9%99%A2')
+        # 三明市采购信息
+        browser.get('http://zfcg.cz.sm.gov.cn/350400/noticelist/e8d2cd51915e4c338dc1c6ee2f02b127/?page=' + str(i + 1) + '&croporgan_name=%E5%8C%BB%E9%99%A2&notice_type=b716da75fe8d4e4387f5a8c72ac2a937')
         # 表的内容
         # 将表的每一行存在table_tr_list中
         table_tr_list = browser.find_element_by_xpath("//table/tbody").find_elements_by_tag_name('tr')
@@ -125,7 +129,7 @@ try:
                     # linkElem = browser.find_element_by_link_text(td.text)
                     # linkElem.click()
     # 保存文件
-    wbk.save('莆田市采购信息.xls')
+    wbk.save('三明市采购信息.xls')
     browser.quit()
 except:
     print('Was not able to find an element with that name.')
